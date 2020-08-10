00806da73b92621bef46b7f8c9f5f92b1e535149
ibrahimfadel2 <69163237+ibrahimfadel2@users.noreply.github.com>
2020-08-10 07:19:08 +0700

    Add files via upload

---
+++ App.kt
@@ -0,0 +1,9 @@
+/*
+Nama    : Fadel Ibrahim
+kelas   : XI RPL 6
+No      : 14
+ */
+
+fun main() {
+    println("Hello Kotlin")
+}
---
+++ hello kotlin.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val name = "fadel"
+    val hobi = "fotografi"
+
+    print("Halo nama saya   :")
+    println(name)
+    print("Hobi saya adalah :")
+    println(hobi)
+    print(if (true)"always true" else "always false")
+
+}
